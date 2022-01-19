8f3d83edbf29c60477d476e99197d073219a5216
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-01-19 19:09:36 +0900

    find_title&link

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
+from os import sep
 from selenium import webdriver
 from bs4 import BeautifulSoup
 import time
@@ -8,5 +9,11 @@ browser.get(url)
 
 html = browser.page_source
 soup = BeautifulSoup(html, "html.parser")
+clips = soup.select('ytd-video-renderer')
 
-print(soup)
+clip = clips[0]
+title = clip.select("#video-title > yt-formatted-string")[0].text
+link = "https://www.youtube.com/"+ clip.select('#video-title')[0]['href']
+print(title,link,sep='\n')
+
+browser.close()
