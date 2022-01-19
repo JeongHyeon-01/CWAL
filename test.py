4b916779c8e9cf0a0ad66dfe44e006b48a9b23e5
Jeonghyeon <89664413+JeongHyeon-01@users.noreply.github.com>
2022-01-19 19:38:45 +0900

    export_excel

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
-from os import sep
 from selenium import webdriver
 from bs4 import BeautifulSoup
+import pandas as pd
 import time
 
 browser = webdriver.Chrome("./chromedriver")
@@ -11,9 +11,14 @@ html = browser.page_source
 soup = BeautifulSoup(html, "html.parser")
 clips = soup.select('ytd-video-renderer')
 
-clip = clips[0]
-title = clip.select("#video-title > yt-formatted-string")[0].text
-link = "https://www.youtube.com/"+ clip.select('#video-title')[0]['href']
-print(title,link,sep='\n')
+results = []
+for clip in clips:
+    title = clip.select("#video-title > yt-formatted-string")[0].text
+    link = "https://www.youtube.com/"+ clip.select('#video-title')[0]['href']
+    
+    data = [title,link]
+    results.append(data)
 
-browser.close()
+df = pd.DataFrame(results)
+df.columns=['영상제목','링크']
+df.to_excel('./populrar.xlsx',index=False)
